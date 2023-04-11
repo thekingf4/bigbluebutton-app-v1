32928ecbd45a541c5142e7a7b8cb626176a4ff08
Luis Capote <lcapote@opensistemas.com>
2023-04-11 12:27:49 -0500

    Update

--- bigbluebuttonapp/urls.py
+++ bigbluebuttonapp/urls.py
@@ -1,14 +1,27 @@
-from django.conf.urls import url
+from django.conf.urls import url, patterns
 from django.conf import settings
-from .views import bigbluebuttonapp_view
+# from .views import bigbluebuttonapp_view
 
 
-urlpatterns = (
+# urlpatterns = (
+#     url(
+#         r'courses/{}/chat$'.format(
+#             settings.COURSE_ID_PATTERN,
+#         ),
+#         bigbluebuttonapp_view,
+#         name='bigbluebuttonapp_view',
+#     ),
+# )
+
+urlpatterns = patterns(
+    'bigbluebuttonapp.views',
+
     url(
         r'courses/{}/chat$'.format(
             settings.COURSE_ID_PATTERN,
         ),
-        bigbluebuttonapp_view.as_view(),
-        name='bigbluebuttonapp_view',
+        'bigbluebuttonapp_view',
+        name='rocketchat_view',
     ),
+
 )
--- bigbluebuttonapp/views.py
+++ bigbluebuttonapp/views.py
@@ -3,17 +3,18 @@ from opaque_keys.edx.keys import CourseKey
 from courseware.courses import get_course_with_access
 from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
 
+
 def bigbluebuttonapp_view(request, course_id):
     import os
     from django.conf import settings
     from path import Path
-    tabTemplatePath = Path( os.path.dirname(os.path.abspath(__file__)) + '/templates' )
-    if settings.MAKO_TEMPLATE_DIRS_BASE[-1] != tabTemplatePath :
+    tabTemplatePath = Path(os.path.dirname(os.path.abspath(__file__)) + '/templates')
+    if settings.MAKO_TEMPLATE_DIRS_BASE[-1] != tabTemplatePath:
         settings.MAKO_TEMPLATE_DIRS_BASE.append(tabTemplatePath)
-    if settings.DEFAULT_TEMPLATE_ENGINE['DIRS'][-1] != tabTemplatePath :
+    if settings.DEFAULT_TEMPLATE_ENGINE['DIRS'][-1] != tabTemplatePath:
         settings.DEFAULT_TEMPLATE_ENGINE['DIRS'].append(tabTemplatePath)
-    tabStaticPath = Path( os.path.dirname(os.path.abspath(__file__)) + '/static' )
-    if settings.STATICFILES_DIRS[-1] != tabStaticPath :
+    tabStaticPath = Path(os.path.dirname(os.path.abspath(__file__)) + '/static')
+    if settings.STATICFILES_DIRS[-1] != tabStaticPath:
         settings.STATICFILES_DIRS.append(tabStaticPath)
     course_key = CourseKey.from_string(course_id)
     course = get_course_with_access(request.user, "load", course_key)
@@ -21,7 +22,7 @@ def bigbluebuttonapp_view(request, course_id):
     context = {
         "course": course,
         "settings": settings,
-        "dirs":  configuration_helpers.get_value('GW_PORTAL_URL', 'PORTAL_URL'),
-        "dirs2":  configuration_helpers.get_value('GW_GCORE_URL', 'G-CORE_URL') ,
+        "dirs": configuration_helpers.get_value('GW_PORTAL_URL', 'PORTAL_URL'),
+        "dirs2": configuration_helpers.get_value('GW_GCORE_URL', 'G-CORE_URL'),
     }
     return render_to_response("bigbluebuttonapp/tab.html", context)
