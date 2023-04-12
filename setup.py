e27c9a040c2a654b1577863906bb5ef6a42f8907
Luis Capote <lcapote@opensistemas.com>
2023-04-12 15:52:29 -0500

    I

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name="bigbluebuttonapp",
-    version="1.0.3",
+    version="1.0.4",
     install_requires=[],
     requires=[],
     packages=["bigbluebuttonapp"],
