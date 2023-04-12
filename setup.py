97d94928e4f5e2a7e952ba5702f60b559227bb97
Luis Capote <luiscapote30@gmail.com>
2023-04-12 15:03:06 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name="bigbluebuttonapp",
-    version="1.0.1",
+    version="1.0.2",
     install_requires=[],
     requires=[],
     packages=["bigbluebuttonapp"],
